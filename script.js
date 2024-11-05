cec0ba8f518ce73e008c7d2a319cc8758c0c709d
Anthony Clarke <anthonydeandreclarke@gmail.com>
2024-11-04 22:38:22 -0600

    created constant variables

--- script.js
+++ script.js
@@ -15,6 +15,17 @@ function onReady() {
     // - Updates state which is ->
     // - Rendered to the DOM
 }
-
-
 onReady()
+// putting HP and AP into an object
+let currentState =[
+    {type: "player", ap: 100}
+    {type: "enemy", hp: 100}
+];
+
+const attacks = [
+    {type: "arcame-scepter" , apCost: 20, hpDamage: 30}
+    {type: "entangle" , apCost: 20, hpDamage: 30}
+    {type: "dragon-blade" , apCost: 20, hpDamage: 30}
+    {type: "star-fire" , apCost: 20, hpDamage: 30}
+]
+
